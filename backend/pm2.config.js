be1bba8571e0e1afb1c05a4c097ee3450eb330d5
Egor Egorov <me@egorfine.com>
2023-11-29 17:40:13 +0100

    pm2

---
+++ backend/pm2.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	apps : [
+    {
+      name: "ts-backend",
+      script: 'index.mjs',
+      kill_timeout: 12 * 1000,
+      restart_delay: 6 * 1000,
+      autorestart: true,
+      max_restarts: 0,
+      watch: false,
+      wait_ready: true,
+
+      env: {
+        TZ: 'UTC',
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
